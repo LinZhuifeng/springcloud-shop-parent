c4db1e3ffd25bdd31de4ebeaf84595615857ea52
2040602490 <2040602490>
2020-08-04 21:18:12 +0800

    实体类修改提交

--- springcloud-shop-manager/springcloud-shop-manager-common/src/main/java/com/shop/entity/AdminRole.java
+++ springcloud-shop-manager/springcloud-shop-manager-common/src/main/java/com/shop/entity/AdminRole.java
@@ -1,11 +1,11 @@
 package com.shop.entity;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter
-@Setter
+@Data
 @ToString
 public class AdminRole {
     private Long adminsId;
